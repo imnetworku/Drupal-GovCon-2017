5e259687c322812dcb932ccd84249713a7f1698d
Bernardo Martinez <israelm28@gmail.com>
2024-06-06 10:42:13 -0400

    Updated settings to allow acquia pipeline to run.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -878,11 +878,11 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-   include $app_root . '/' . $site_path . '/settings.local.php';
-}
+// if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+//    include $app_root . '/' . $site_path . '/settings.local.php';
+// }
 
-// require DRUPAL_ROOT . "/../vendor/acquia/drupal-recommended-settings/settings/acquia-recommended.settings.php";
+require DRUPAL_ROOT . "/../vendor/acquia/drupal-recommended-settings/settings/acquia-recommended.settings.php";
 /**
  * IMPORTANT.
  *
