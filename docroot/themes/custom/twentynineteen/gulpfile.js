eaed8e263872f9fce526a185deb6d3fd91cbd096
Bernardo Martinez <israelm28@gmail.com>
2024-06-08 01:12:58 -0400

    Cleans up gulpfile.js

--- docroot/themes/custom/twentynineteen/gulpfile.js
+++ docroot/themes/custom/twentynineteen/gulpfile.js
@@ -7,7 +7,6 @@ var rename = require('gulp-rename');
 var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
 var cleanCSS = require('gulp-clean-css');
-// var del = require('del');
 
 var paths = {
   styles: {
@@ -21,16 +20,6 @@ var paths = {
   images: 'img/',
   styleGuide: 'styleguide'
 };
-
-/* Not all tasks need to use streams, a gulpfile is just another node program
- * and you can use all packages available on npm, but it must return either a
- * Promise, a Stream or take a callback and call it
- */
-function clean() {
-  // You can use multiple globbing patterns as you would with `gulp.src`,
-  // for example if you are using del 2.0 or above, return its promise
-  // return del([ 'assets' ]);
-}
 
 /*
  * Define our tasks using plain functions
@@ -61,7 +50,6 @@ var build = gulp.parallel(styles, scripts);
 /*
  * You can use CommonJS `exports` module notation to declare tasks
  */
-// exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.build = build;
